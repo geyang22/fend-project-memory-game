c4a3a12df8f60d9eab0512beb233f6c541d6fe3b
Magda S <your-email-address>
2018-07-31 18:59:12 +0200

    Final improvements

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
+"use strict";
 /*
  * Create a list that holds all of your cards
  */
-const deck = ['fa-diamond', 'fa-diamond',
-  'fa-paper-plane-o', 'fa-paper-plane-o',
-  'fa-anchor', 'fa-anchor',
-  'fa-bolt', 'fa-bolt',
-  'fa-cube', 'fa-cube',
-  'fa-leaf', 'fa-leaf',
-  'fa-bicycle', 'fa-bicycle',
-  'fa-bomb', 'fa-bomb'
+const deck = [
+  "fa-diamond",
+  "fa-diamond",
+  "fa-paper-plane-o",
+  "fa-paper-plane-o",
+  "fa-anchor",
+  "fa-anchor",
+  "fa-bolt",
+  "fa-bolt",
+  "fa-cube",
+  "fa-cube",
+  "fa-leaf",
+  "fa-leaf",
+  "fa-bicycle",
+  "fa-bicycle",
+  "fa-bomb",
+  "fa-bomb"
 ];
 
 /*
@@ -20,8 +30,9 @@ const deck = ['fa-diamond', 'fa-diamond',
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue, randomIndex;
+  let currentIndex = array.length;
+  let temporaryValue = "";
+  let randomIndex = "";
 
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
@@ -35,10 +46,10 @@ function shuffle(array) {
 }
 
 // Declare variables needed
-const moves = document.querySelector('.moves');
-const stars = document.querySelectorAll('.fa-star');
-const popup = document.querySelectorAll('.popup')[0];
-const list = document.querySelector('.deck');
+const moves = document.querySelector(".moves");
+const stars = document.querySelectorAll(".fa-star");
+const popup = document.querySelectorAll(".popup")[0];
+const list = document.querySelector(".deck");
 // An array that stores all the matched cards to check when the game is over
 let match = [];
 
@@ -58,7 +69,6 @@ function gameStart() {
   // Emptying the match array
   match = [];
 
-
   // Remove the current deck, create a new deck
   if (list.childNodes.length !== 0) {
     while (list.firstChild) {
@@ -74,31 +84,37 @@ function gameStart() {
   // A function to loop through each card and create its HTML
   function createDeck() {
     deck.forEach(function(card) {
-      const item = document.createElement('li');
+      const item = document.createElement("li");
       list.appendChild(item);
       item.className = "card";
-      const item1 = document.createElement('i');
+      const item1 = document.createElement("i");
       item.appendChild(item1);
       item1.classList.add("fa", card);
     });
   }
 
   // Flipping the cards on a click
-  const allCards = document.querySelectorAll('.card');
+  const allCards = document.querySelectorAll(".card");
   let cards = [];
   allCards.forEach(function(card) {
-    card.addEventListener('click', function(e) {
+    card.addEventListener("click", function(e) {
       // Condition for when a card is clicked twice
-      if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
-        card.classList.add('open', 'show');
+      if (!card.classList.contains("open") &&
+        !card.classList.contains("show") &&
+        !card.classList.contains("match")
+      ) {
+        card.classList.add("open", "show");
         cards.push(card);
         // Leave open in case of match
         if (cards.length == 2) {
           const card1 = cards[0];
           const card2 = cards[1];
-          if (card1.childNodes[0].classList[1] === card2.childNodes[0].classList[1]) {
-            card1.classList.add('match');
-            card2.classList.add('match');
+          if (
+            card1.childNodes[0].classList[1] ===
+            card2.childNodes[0].classList[1]
+          ) {
+            card1.classList.add("match");
+            card2.classList.add("match");
             // Push into the array that stores all matched cards to know when the game is over
             match.push(card1);
             match.push(card2);
@@ -108,37 +124,34 @@ function gameStart() {
           else {
             setTimeout(function() {
               cards.forEach(function(card) {
-                card.classList.remove('open', 'show');
+                card.classList.remove("open", "show");
               });
               cards = [];
             }, 1000);
           }
         }
-
-
       }
 
       // Update number of Moves
-      num += 1;
-      moves.innerHTML = num;
+      num += 0.5;
+      moves.innerHTML = parseInt(num);
 
       // Stars rating
       let starsCount = 3;
-      if (num > 25 && num < 35) {
+      if (num > 15 && num < 30) {
         stars[2].style.display = "none";
         starsCount = 2;
-      } else if (num > 34) {
+      } else if (num > 29) {
         stars[2].style.display = "none";
         stars[1].style.display = "none";
         starsCount = 1;
       }
 
-
       // Winning
       const minutes = document.querySelector(".minutes");
       const seconds = document.querySelector(".seconds");
       const starsRating = document.querySelector(".stars");
-      if (match.length == 2) {
+      if (match.length == 16) {
         minutes.innerHTML = minute;
         seconds.innerHTML = second;
         starsRating.innerHTML = starsCount;
@@ -156,11 +169,10 @@ let minute = "";
 
 function count() {
   time += 1;
-  second = (time % 60);
+  second = time % 60;
   minute = parseInt(time / 60);
-  document.querySelectorAll('#timer')[0].innerHTML = minute + ":" + second;
+  document.querySelectorAll("#timer")[0].innerHTML = minute + ":" + second;
 }
-
 
 gameStart();
 
@@ -175,13 +187,16 @@ gameStart();
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 // Play again
-const playAgain = document.querySelector('.button');
-playAgain.addEventListener('click', function(e) {
+const playAgain = document.querySelector(".button");
+playAgain.addEventListener("click", function(e) {
   gameStart();
+  // window.clearInterval(timeCount);
+  // time = 0;
+  // const timeCount = setInterval(count, 1000);
 });
 
 // Restart button
-const restart = document.querySelector('.restart');
-restart.addEventListener('click', function(e) {
+const restart = document.querySelector(".restart");
+restart.addEventListener("click", function(e) {
   gameStart();
 });
